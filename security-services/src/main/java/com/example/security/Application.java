0390ee7f4bd8f839439c359b6a9500aba53c97d7
Mohit Arora <mailtomohitarora@gmail.com>
2015-01-24 14:04:07 -0500

    Added Identity Controller for getting a unique id for session

--- security-services/src/main/java/com/example/security/Application.java
+++ security-services/src/main/java/com/example/security/Application.java
@@ -3,9 +3,7 @@ package com.example.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
