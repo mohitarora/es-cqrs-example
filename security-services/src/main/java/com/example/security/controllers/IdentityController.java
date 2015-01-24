fd4f62478a5d6efb37e7d54c2c7f9c7f1a26bc53
Mohit Arora <mailtomohitarora@gmail.com>
2015-01-24 13:57:22 -0500

    Added identity controller

--- security-services/src/main/java/com/example/security/controllers/IdentityController.java
+++ security-services/src/main/java/com/example/security/controllers/IdentityController.java
@@ -3,12 +3,14 @@ package com.example.security.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class IdentityController {
 
     @RequestMapping("/")
-    String id() {
-        return "Hello Id";
+    public String id() {
+        return UUID.randomUUID().toString();
     }
 
 }
