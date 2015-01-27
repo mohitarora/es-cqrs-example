556ddf7d9895f5081b7536948eed04b508e840b0
Mohit Arora <mailtomohitarora@gmail.com>
2015-01-26 23:33:46 -0500

    Added end point for product details

--- catalog-services/src/main/java/com/example/catalog/controllers/ProductsController.java
+++ catalog-services/src/main/java/com/example/catalog/controllers/ProductsController.java
@@ -4,19 +4,26 @@ import com.example.catalog.domain.Product;
 import com.example.catalog.repository.ProductsRepository;
 import org.elasticsearch.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController {
 
     @Autowired
     private ProductsRepository productsRepository;
 
-    @RequestMapping("/products")
+    @RequestMapping
     public List<Product> products() {
         return Lists.newArrayList(productsRepository.findAll());
+    }
+
+    @RequestMapping(value = "/{id}")
+    public Product product(@PathVariable String id) {
+        return productsRepository.findOne(id);
     }
 }
