60a7e9693d491cefa8112e16034271dff5e1f964
Mohit Arora <mailtomohitarora@gmail.com>
2015-02-07 22:24:09 -0500

    Added CartController

--- cart-command-services/src/main/java/com/example/cart/Application.java
+++ cart-command-services/src/main/java/com/example/cart/Application.java
@@ -2,12 +2,24 @@ package com.example.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
 
 @SpringBootApplication
+@ImportResource("classpath:kafka.xml")
 public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
+//    @MessagingGateway
+//    public interface KafkaGateway {
+//
+//        @Gateway(requestChannel = "addItem.input")
+//        public void addItemTocart();
+//
+//    }
 
 }
---
+++ cart-command-services/src/main/java/com/example/cart/controller/CartController.java
@@ -0,0 +1,20 @@
+package com.example.cart.controller;
+
+import com.example.cart.Application.KafkaGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private KafkaGateway kafkaGateway;
+
+    @RequestMapping
+    public void addItem() {
+        kafkaGateway.addItemTocart();
+    }
+
+}
