d409ebc753ef09432c0d7d21f53deacd929b7805
Mohit Arora <mailtomohitarora@gmail.com>
2015-02-23 22:18:59 -0500

    Added integration flow for cart query side

--- cart-query-services/src/main/java/com/example/cart/Application.java
+++ cart-query-services/src/main/java/com/example/cart/Application.java
@@ -2,8 +2,11 @@ package com.example.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
 
 @SpringBootApplication
 @IntegrationComponentScan
@@ -12,6 +15,16 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Spring integration flow for messages received from Kafka using Spring Integration DSL.
+     */
+    @Bean
+    public IntegrationFlow myFlow() {
+        return IntegrationFlows.from("kafkaInboundChannel")
+                .handle(System.out::println)
+                .get();
     }
 
 }
