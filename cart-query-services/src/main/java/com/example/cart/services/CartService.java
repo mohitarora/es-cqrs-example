ed60131d33e5498fe3c94850a9fea44f30e1833a
Mohit Arora <mailtomohitarora@gmail.com>
2015-04-05 00:12:32 -0400

    Removed unncessary looping

--- cart-query-services/src/main/java/com/example/cart/services/CartService.java
+++ cart-query-services/src/main/java/com/example/cart/services/CartService.java
@@ -1,10 +1,7 @@
 package com.example.cart.services;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class CartService {
 
@@ -12,18 +9,9 @@ public class CartService {
      * Start Processing Kafka Messages from the channel
      */
     public void process(Map<String, Map<Integer, List<byte[]>>> kafkaMessages) {
-        for (Map.Entry entry : kafkaMessages.entrySet()) {
-            String topic = (String) entry.getKey();
-            System.out.println("Topic:" + topic);
-            ConcurrentHashMap<Integer, List<byte[]>> messages = (ConcurrentHashMap<Integer, List<byte[]>>) entry.getValue();
-            Collection<List<byte[]>> values = messages.values();
-            for (Iterator<List<byte[]>> iterator = values.iterator(); iterator.hasNext(); ) {
-                List<byte[]> list = iterator.next();
-                for (byte[] object : list) {
-                    String message = new String(object);
-                    System.out.println("\tMessage: " + message);
-                }
-            }
+        for (Map.Entry<String, Map<Integer, List<byte[]>>> entry : kafkaMessages.entrySet()) {
+            System.out.println("Topic:" + entry.getKey());
+            entry.getValue().values().stream().forEach(System.out::println);
         }
     }
 }
